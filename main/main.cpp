40f50110224bcf50d07832929f2fc82b065155c6
Youthazmit1 <104276414+Youthazmit1@users.noreply.github.com>
2022-12-07 12:53:45 -0500

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -1,1 +1,12 @@
+// Import hell
 
+
+// Starts in setup()
+static Engine *engine = nullptr;
+static ProjectSettings *global = nullptr;
+static Input *input = nullptr;
+static InputMap *input_map = nullptr;
+static TranslationServer *translation_server = nullptr;
+static Performance *performance = nullptr;
+static PackedData *packed_data = nullptr;
+static Time *time_singleton = nullptr;
