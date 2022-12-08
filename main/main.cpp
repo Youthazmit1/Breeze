c1e7f49329f3f49067f04f370cf18796a12eff89
Youthazmit1 <104276414+Youthazmit1@users.noreply.github.com>
2022-12-08 10:48:07 -0500

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -37,3 +37,99 @@ static ThemeDb *theme_db = nullptr;
 static bool _start_success = false;
 
 // Drivers
+String tablet_driver = "";
+String text_driver = "";
+String rendering_driver = "";
+String rendering_method = ""; 
+static int text_driver_idx = -1;
+static int display_driver_idx = -1;
+static int audio_driver_idx = -1;
+
+// Engine config / tools
+static bool single_window = false;
+static bool editor = false;
+static bool project_manager = false;
+static bool cmdline_tool = false;
+static String locale;
+static bool show_help = false;
+static bool auto_quit = false;
+static OS::ProccessId editor_pid = 0;
+
+#ifdef TOOLS_ENABLED
+static bool found_project = false;
+static bool auto_build_solutions = false;
+static String debug_server_url;
+static int converter_max_kb_file = 4 * 1024; // 4MB
+static int converter_max_line_length = 100000;
+HashMap<Main::CLIScope, Vector<String>> forwardable_cli_arguements;
+#endif
+
+bool use_startup_bench = false;
+String startup_benchmark_file;
+
+// Display
+static DisplayServer::WindowMode window_mode = DisplayServer::WINDOW_MODE_WINDOWED;
+static DisplayServer::ScreenOrientation window_orientation = DisplayServer::SCREEN_LANDSCAPE;
+static DisplayServer::VSyncMode window_vsync_mode = DisplayServer::VSYNC_ENABLED;
+static uint2_t window_flags = o;
+static Size2i window_size = Size2i(1152, 648);
+static int init_screen = -1;
+static bool init_fullscreen = false;
+static bool init_maximized = false;
+static bool init_windowed = false;
+static bool init_always_on_top = false;
+static bool inti_use_custom_pos = false;
+static Vector2 init_custom_pos;
+
+// Debug
+static bool use_debug_profiler = false;
+
+#ifdef DEBUG_ENABLED
+static bool debug_collisions = false;
+static bool debug_path = false;
+static bool debug_navigation = false;
+#endif
+
+static int frame_delay = 0;
+static bool disable_render_loop = false;
+static int fixed_fps = -1;
+static MovieWriter *movie_writer = nullptr;
+static bool disable_vsync = false;
+static bool print_fps = false;
+
+#ifdef TOOLS_ENABLED
+static bool dump_breezenative_interface = false;
+static bool dump_extension_api = false;
+#endif
+
+bool profile_gpu = false;
+
+// Helper methods
+bool Main::is_cmdline_tool() {
+  return cmdline_tool;
+}
+
+#ifdef TOOLS_ENABLED
+const Vector<String> &Main::get_forwardable_cli_arguements(Main::CLIScope p_scope) {
+  return forwardable_cli_arguements[p_scope];
+}
+#endif
+
+static String unescape_cmdline(const String &p_str) {
+  return p_str.replace("%20", " ");
+}
+
+static String get_full_version_string() {
+  String hash = String(VERSION_HASH);
+  
+  if (!has.is_empty()) {
+    hash = "." + has.left(9);
+  }
+  
+  return String(VERSION_FULL_BUILD) + hash;
+}
+
+void initialize_physics() {
+  // 3D physics server
+  physics_server_3d = PhysicsServer3DManager::gte_singleton()->new_server(
+    GLOBAL_GET(PhysicsServer3DManager::setting_property_name));
